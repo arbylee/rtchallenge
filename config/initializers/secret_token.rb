f16fb8d0377076e26913cb662652dd567c5dd521
Richard Lee <rblee88@gmail.com>
2014-03-18 07:57:03 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rtgame::Application.config.secret_key_base = '8a389f33f0b5b8eadfdaa582e28ad150ab04f4ac8d7b4bc5c16ef625a23f90b79dff9204da2856ddbd6c05d1ffdb87685dcfe85391bd05924bcdb77621eadcef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rtgame::Application.config.session_store :cookie_store, key: '_rtgame_session'
