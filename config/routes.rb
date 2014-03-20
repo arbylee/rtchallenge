03ad7bfea1c871dfa591c7c86e2375814a5d6243
Richard Lee <rblee88@gmail.com>
2014-03-19 21:07:03 -0500

    Root to Challenges index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rtgame::Application.routes.draw do
   resources :challenges
 
   devise_for :users
+  root 'challenges#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
