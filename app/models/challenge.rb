ef5cf539e0354bf520ae031836e98a79866c2191
Richard Lee <rblee88@gmail.com>
2014-03-19 21:24:17 -0500

    Add Challenge associations

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,7 @@
 class Challenge < ActiveRecord::Base
+  has_many :challenges_users
+  has_many :users, through: :challenges_users
+  has_many :challenges_movies
+  has_many :movies, through: :challenges_movies
+  has_many :reviews
 end
