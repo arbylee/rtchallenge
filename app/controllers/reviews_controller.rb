3e6f62026561ecbaf54b1d8dd70cdb4846e857e7
Richard Lee <rblee88@gmail.com>
2014-03-20 21:15:42 -0500

    Logged in user can create reviews for a challenge

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,16 @@
 class ReviewsController < ApplicationController
+  before_filter :authenticate_user!
+
   def create
+    reviews = params['reviews']
+    reviews.each do |index, rev|
+      if rev['score'].present?
+        movie = Movie.find_or_create_by_rt_id(rev['movie_id'])
+        review = Review.find_or_initialize_by(user: current_user, movie: movie)
+        review.score = rev['score']
+        review.save
+      end
+    end
     redirect_to challenges_path
   end
 end
