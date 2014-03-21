15f6477da6cf25353c57bdfca1528df87a5e6047
Richard Lee <rblee88@gmail.com>
2014-03-21 13:50:28 -0500

    Can now create challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -12,4 +12,18 @@ class ChallengesController < ApplicationController
                                                movie: movie)
     end
   end
+
+  def new
+    @challenge = Challenge.new
+    @users = User.all
+    @movies = Movie.all
+  end
+
+  def create
+    @challenge = Challenge.new
+    @challenge.movie_ids = params['challenge']['movie_ids']
+    @challenge.user_ids = params['challenge']['user_ids']
+    @challenge.save
+    redirect_to challenges_path
+  end
 end
