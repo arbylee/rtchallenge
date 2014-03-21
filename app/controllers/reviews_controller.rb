2c41b915ea733074096748c8595ea2559ffd5e1d
Richard Lee <rblee88@gmail.com>
2014-03-21 00:01:41 -0500

    Save the challenge_id to the review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,10 +3,13 @@ class ReviewsController < ApplicationController
 
   def create
     reviews = params['reviews']
+    challenge_id = params['challenge_id']
     reviews.each do |index, rev|
       if rev['score'].present?
-        movie = Movie.where(rt_id: rev['movie_id'])
-        review = Review.find_or_initialize_by(user: current_user, movie: movie)
+        movie = Movie.find(rev['movie_id'])
+        review = Review.find_or_initialize_by(user_id: current_user.id,
+                                              movie_id: movie.id,
+                                              challenge_id: challenge_id)
         review.score = rev['score']
         review.save
       end
