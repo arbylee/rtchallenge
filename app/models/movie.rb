ab830950c466ec9c03a88cc1e7ee1cdfc5b2bd46
Richard Lee <rblee88@gmail.com>
2014-03-21 00:35:55 -0500

    Give a link to the movie trailer

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,17 @@ class Movie < ActiveRecord::Base
   def rt_movie
     @rt_movie ||= RottenMovie.find(id: self.rt_id)
   end
+
+  def clips
+    if !@clips
+      clips_url = URI.parse(rt_movie.links.clips + "?apikey=#{Rotten.api_key}")
+      resp = Net::HTTP.get_response(clips_url)
+      @clips = JSON(resp.body)['clips']
+    end
+    return @clips
+  end
+
+  def trailer_link
+    clips.first['links']['alternate']
+  end
 end
